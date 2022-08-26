effc4a8d2b16f1814cb73d5b2fb2636738d7f70e
Nariman <87778834+Nariman-R@users.noreply.github.com>
2022-08-26 18:16:42 +0000

    done tasks from lessons 2 and 3

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,14 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  let sortOrder = 1;
+  if (param=='desc') {
+    sortOrder = -1;
+  }
+  
+  let compareArrayElements = (string1, string2) => {
+    return sortOrder * string1.localeCompare(string2, ['ru','en'], {caseFirst: 'upper'});
+  };
 
+  return [...arr].sort(compareArrayElements);
 }
--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathElements = path.split('.');
 
+  const getter = function(obj) {
+      let result = obj;
+
+      const getElement = function(index) {
+            if (index ===pathElements.length || result === undefined) {
+              return result;
+            }
+            result = result[pathElements[index]]
+          return getElement(index+1);
+    }
+    return getElement(0);
+  }
+  return getter
 }
